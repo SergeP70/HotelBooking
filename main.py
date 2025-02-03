c10f08d825e90e815a09731f305f1e145911703f
sergeP70 <serge@pille.be>
2025-02-03 07:23:20 +0100

    Implement the methods of the Reservation class

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class User:
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name = df.loc[df['id'] == hotel_id, 'name'].squeeze()
 
     def book(self):
         # Book a hotel by changing its availability to no
@@ -28,11 +29,18 @@ class Hotel:
 
 
 class Reservation:
-    def __init__(self, name, hotel):
-        pass
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
 
     def generate(self):
-        content = f"Name of the customer hotel"
+        content = f"""
+        Thank you for you reservation!
+        Here are your booking data:
+        Name: {self.customer_name}
+        Hotel: {self.hotel.name}
+        """
         return content
 
 
@@ -43,7 +51,7 @@ hotel = Hotel(hotel_id)
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
-    reservation = Reservation(name, hotel)
+    reservation = Reservation(customer_name=name, hotel_object=hotel)
     print(reservation.generate())
 else:
     print("Hotel is not free")
